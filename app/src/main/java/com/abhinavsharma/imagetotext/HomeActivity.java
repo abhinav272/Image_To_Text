3beb4976220966368585b2c8702fe2f28f48c179
Abhinav Sharma <abhinav.sharma@getvoodoo.in>
2016-07-15 17:15:15 +0530

    Preference code updated

--- app/src/main/java/com/abhinavsharma/imagetotext/HomeActivity.java
+++ app/src/main/java/com/abhinavsharma/imagetotext/HomeActivity.java
@@ -130,7 +130,6 @@ public class HomeActivity extends AppCompatActivity implements View.OnClickListe
             if (al != null && al.size() > 0) {
                 Intent intent = new Intent(HomeActivity.this, TextViewer.class);
                 intent.putExtra("all_text", al);
-                Log.e(TAG, "onActivityResult: Size " + al.size());
                 startActivity(intent);
             } else {
                 showNoDataDialog();
